661ca81812fa2c4a7bc9491a44503f870692d0bf
Krisda Suwankarn <suwankarn@gmail.com>
2026-03-19 23:41:53 +0700

    feat: initial commit

---
+++ app/(landing)/Footer.tsx
@@ -0,0 +1,5 @@
+export default function Footer() {
+  return (
+    <div>&copy; 2024 Your Company. All rights reserved.</div>
+  )
+}
---
+++ app/(landing)/page.tsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  const name = "World"
+
+  return (
+    <>
+      <h1 className="text-3xl text-amber-600 animate-bounce">Hello, {name}!</h1>
+    </>
+  )
+}
